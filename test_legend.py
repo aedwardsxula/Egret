48ddca69a1f6128d0cc9736e40657f481f7928f9
Robin <robinhallct@gmail.com>
2025-11-09 21:11:32 -0600

    Test skip list lads empty file

--- test_legend.py
+++ test_legend.py
@@ -8,13 +8,18 @@ class TestSynonymProcessor(unittest.TestCase):
     def test_initialization_defaults(self):
         self.assertTrue(self.processor.noun_flag)
         self.assertIsInstance(self.processor.skip, set)
-        self.assertIsInstance(self.processor.session, object)  # Could be requests.Session
+        self.assertIsInstance(self.processor.session, object)
         self.assertEqual(self.processor.skiptext, 0)
         self.assertEqual(self.processor.tochange, 0)
     
     def test_load_skip_list_creates_skip_set(self):
         self.processor.load_skip_list("test_skip.txt")
         self.assertIn("example", self.processor.skip)
+
+    def test_load_skip_list_empty_file(self):
+        self.processor.load_skip_list("empty_skip.txt")  
+        self.assertEqual(len(self.processor.skip), 0)
+
 
     def test_process_sentence_returns_expected_output(self):
         sentence = "The quick brown fox jumps over the lazy dog."
