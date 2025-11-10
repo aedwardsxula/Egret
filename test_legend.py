6995d7811dba22d7a47c6baa50ab73fb9fa4b191
Robin <robinhallct@gmail.com>
2025-11-09 21:04:48 -0600

    Test initialization defaults

--- test_legend.py
+++ test_legend.py
@@ -4,4 +4,10 @@ from synonym_processor import SynonymProcessor
 class TestSynonymProcessor(unittest.TestCase):
     def setUp(self):
         self.processor = SynonymProcessor()
-        
+
+    def test_initialization_defaults(self):
+        self.assertTrue(self.processor.noun_flag)
+        self.assertIsInstance(self.processor.skip, set)
+        self.assertIsInstance(self.processor.session, object)  # Could be requests.Session
+        self.assertEqual(self.processor.skiptext, 0)
+        self.assertEqual(self.processor.tochange, 0)
